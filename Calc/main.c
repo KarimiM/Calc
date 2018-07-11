7d7e314fd6eb77bc854a05f2a0e3981709cb0a1f
Masood Karimi <masoodkarimi@nbp-236-115.nbp.ruw.rutgers.edu>
2018-07-11 18:10:05 -0400

    Initial Commit

---
+++ Calc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Calc
+//
+//  Created by Masood Karimi on 7/11/18.
+//  Copyright © 2018 Masood Karimi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
